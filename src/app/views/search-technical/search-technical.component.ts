1f6a70df2d07ae96d40b5b0335a61951350427c4
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-09-24 11:50:38 -0500

    feat(searchTechnical): added dependencies for component

---
+++ src/app/views/search-technical/search-technical.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-technical',
+  templateUrl: './search-technical.component.html',
+  styleUrls: ['./search-technical.component.css']
+})
+export class SearchTechnicalComponent {
+
+}
