6903d342a9abdcf1304dfe904199ceb151f82f2a
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-09-24 11:52:44 -0500

    feat(searchTechnical): fixed dependencies modules on sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { SearchTechnicalComponent } from './views/search-technical/search-technical.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SearchTechnicalComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
