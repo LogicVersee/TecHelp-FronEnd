92c6658b87fc188cbc7e29de2a80a3db152c8414
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-09-23 20:23:28 -0500

    feat(register): added register component dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { RegisterComponent } from './views/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
