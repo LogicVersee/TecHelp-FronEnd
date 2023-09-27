3f38197b1fafb413d1e0c0ca9ab5be837fb3e39d
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2023-09-27 01:26:54 -0500

    fix(routes): fices routes between components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SearchTechnicalComponent} from "./views/search-technical/search-technical.component";
 import {TechProfileComponent} from "./views/tech-profile/tech-profile.component";
+import {HomeComponent} from "./components/home/home.component";
+import {RequestComponent} from "./components/request/request.component";
 
 const routes: Routes = [
   {path: 'searchTechnicals', component: SearchTechnicalComponent},
+  {path: 'home', component: HomeComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'techProfile/:id', component: TechProfileComponent}
+  {path: 'techProfile/:id', component: TechProfileComponent},
+  {path: 'request', component: RequestComponent}
 ];
 
 @NgModule({
