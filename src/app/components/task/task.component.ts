1f67c9ce093aa51cbcdf438230d3b43b41c5576a
Mirroy25 <112901601+Mirroy25@users.noreply.github.com>
2023-10-27 17:57:05 -0500

    feat(frontend): add filter

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,23 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from "../../interfaces/task";
+import {MatTableDataSource} from "@angular/material/table";
 
 
-const listTask: Task[] = [
-  {id: 1, name: 'Luis', cellphone: 'Motorola', problem: 'crash', element: 'batery', income:'60'},
 
-];
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit{
+
+  listTask: Task[] = [
+    {id: 1, name: 'Luis', cellphone: 'Motorola', problem: 'crash', element: 'batery', income:'60'},
+    {id: 2, name: 'Sandra', cellphone: 'Motorola', problem: 'crash', element: 'batery', income:'40'},
+  ];
+
+
   displayedColumns: string[] = ['id', 'name', 'cellphone', 'date','action'];
-  dataSource = listTask;
+  dataSource = new MatTableDataSource(this.listTask);
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
