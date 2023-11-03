ddadc6fbc00c96ff584dd4bca8deda6ec2867fce
Mirroy25 <112901601+Mirroy25@users.noreply.github.com>
2023-11-03 13:17:47 -0500

    feat(tasks): added service facade for students endpoint.

---
+++ src/app/tasking/model/task.ts
@@ -0,0 +1,20 @@
+export class Task {
+    id:number;
+    client_name:string;
+    phone_name:string;
+    problem:string;
+    components_to_use:string;
+    value_progress:number;
+    delivery_day:string;
+    income:string;
+ constructor() {
+ this.id=0;
+ this.client_name="";
+ this.phone_name="";
+ this.problem="";
+ this.components_to_use="";
+ this.value_progress=0;
+ this.delivery_day="";
+ this.income="";
+ }
+}
