94a372f797de4fad43bd917ba99adaee11d58ded
Janover Saldaña <u20201b510@upc.edu.pe>
2023-11-03 23:32:01 -0500

    feat(security): Implemented page not found.

--- src/app/shared/pages/page-not-found/page-not-found.component.ts
+++ src/app/shared/pages/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent {
+  invalidUrl: string = '';
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
+  ngOnInit(): void {
+    this.invalidUrl = this.route.snapshot.url[0].path;
+  }
+
+  navigateToHome() {
+    this.router.navigate(['login']);
+  }
 }
